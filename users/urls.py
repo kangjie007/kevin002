9f0f054126623ee7f5c11965e0eb14b5845ce942
kangjie007 <jokw@foxmail.com>
2019-03-14 23:49:52 +0800

    firstdone

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+from .import views
+app_name='users'
+
+urlpatterns =[
+    #登录页面
+    path('login/',LoginView.as_view(template_name='users/login.html'),name='login'),
+    # 注销
+   # path('logout/', views.logout_view, name='logout'),
+# Logout page.
+    path('logout/', views.logout_view, name='logout'),
+# 注册页面
+    path('register/', views.register, name='register'),
+]
